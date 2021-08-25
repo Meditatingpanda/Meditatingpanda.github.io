5697b13af106c55dd312b54925217a9572312f7c
Gyana Ranjan panda <83230804+lonewolf2002@users.noreply.github.com>
2021-08-25 20:27:03 +0530

    Add files via upload

---
+++ light_port/coding.js
@@ -0,0 +1,11 @@
+var animation = bodymovin.loadAnimation({
+    container: document.getElementById('codingAnimation'),
+        
+   
+    renderer: 'svg',
+    loop: true,
+    autoplay: true,
+    path: 'Blogging.json'
+        
+   
+    })
